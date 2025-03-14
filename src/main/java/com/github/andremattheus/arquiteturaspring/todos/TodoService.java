986ef56dd414e97ec0db6f70ea85d5bd33148dce
Matheus André <Andre.Mattheusc@gmail.com>
2025-03-14 20:17:04 -0300

    Update EntityController Learning Project

---
+++ src/main/java/com/github/andremattheus/arquiteturaspring/todos/TodoService.java
@@ -0,0 +1,25 @@
+package com.github.andremattheus.arquiteturaspring.todos;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+
+    private TodoRepository repository;
+
+    public TodoService(TodoRepository repository) {
+        this.repository = repository;
+    }
+
+    public TodoEntity salvar(TodoEntity novoTodo){
+        return repository.save(novoTodo);
+    }
+
+    public void atualizarStatus(TodoEntity todo){
+        repository.save(todo);
+    }
+
+    public TodoEntity buscarPorId(Integer id){
+        return repository.findById(id).orElse(null);
+    }
+}
